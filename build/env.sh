4e212418c2abfe864d524dd46c0d0b074f6f9912
Lindsey Gray <lindsey.gray@gmail.com>
2021-01-06 23:36:16 -0600

    env patch

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/sammy007"
-if [ ! -L "$ethdir/open-ethereum-pool" ]; then
-    mkdir -p "$ethdir"
-    cd "$ethdir"
-    ln -s ../../../../../. open-ethereum-pool
+ovldir="$workspace/src/github.com/lgray"
+if [ ! -L "$ovldir/open-overline-pool" ]; then
+    mkdir -p "$ovldir"
+    cd "$ovldir"
+    ln -s ../../../../../. open-overline-pool
     cd "$root"
 fi
 
@@ -25,8 +25,8 @@ GOBIN="$PWD/build/bin"
 export GOPATH GOBIN
 
 # Run the command inside the workspace.
-cd "$ethdir/open-ethereum-pool"
-PWD="$ethdir/open-ethereum-pool"
+cd "$ovldir/open-overline-pool"
+PWD="$ovldir/open-overline-pool"
 
 # Launch the arguments with the configured environment.
 exec "$@"
