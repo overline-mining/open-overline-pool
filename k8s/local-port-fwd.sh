337f52cead2c26a8e5201924532d61140f351043
Lindsey Gray <lindsey.gray@gmail.com>
2021-01-09 19:00:37 -0600

    working api/webserver with ethereum, tweak bcnode svc

--- k8s/local-port-fwd.sh
+++ k8s/local-port-fwd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 trap 'kill %1;' SIGINT
-kubectl port-forward open-ov-pool-frontend 8080 7020 &
-kubectl port-forward open-ov-pool-api 6283 3141 3142
+kubectl port-forward open-overline-pool-frontend 8080 7020 &
+kubectl port-forward open-overline-pool-api 6283 3141 3142
