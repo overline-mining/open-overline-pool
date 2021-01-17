6b7971f8975d0c3991bbef6b97e03a27b734e172
Lindsey Gray <lindsey.gray@gmail.com>
2021-01-17 02:06:21 -0600

    tweaks to k8s

--- bomb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -d '{"jsonrpc":"2.0","id":0,"method":"stats","params":[]}' \
-     -H 'Content-Type: application/json' \
-     -u ':correct-horse-battery-staple' http://overline:3000/rpc
-
-echo
--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 curl -d '{"jsonrpc":"2.0","id":0,"method":"getSyncStatus","params":[]}' \
      -H 'Content-Type: application/json' \
-     -u ':correct-horse-battery-staple' http://overline:3000/rpc --verbose
+     -u ':correct-horse-battery-staple' http://localhost:3000/rpc --verbose
 
 echo
