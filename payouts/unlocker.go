8b59a5ce355d34280b286a0c60137039b46e2535
p0ncho <37346522+p0nch00@users.noreply.github.com>
2021-11-29 09:55:47 +0100

    Update unlocker.go
    weird behaviour change on 1.4.9 nodes, resulting in needing to restart unlocker

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -351,7 +351,7 @@ func (u *BlockUnlocker) unlockAndCreditMiners() {
 
 	current, err := u.rpc.GetLatestBlock()
 	if err != nil {
-		u.halt = true
+		//u.halt = true #BCnode after 1.4.9 is weird. They do EOF when they sync to a fork, instead of just delaying
 		u.lastFail = err
 		log.Printf("Unable to get current blockchain height from node: %v", err)
 		return
